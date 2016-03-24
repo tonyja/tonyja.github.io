c4a1d57e7f61580c8578b5df46d6e0b5e9925bbb
Tony Jackson <tjackson@yammer-inc.com>
2016-03-24 13:49:55 -0700

    Disable the error reporting that is no longer compatible

--- yamdebug/yd_bootstrap.js
+++ yamdebug/yd_bootstrap.js
@@ -300,7 +300,8 @@ yd.a.mdl.F.prototype.toString = function (verbose) {
                        console.log(yd.logd('RGC tow_use_unviewed_ocular_counts_v2: ' + cu.treatments.tow_use_unviewed_ocular_counts_v2));
                        console.log(yd.logd('ADUX tow_assured_delivery_v2: ' + cu.treatments.tow_assured_delivery_v2));
                        console.log(yd.logd('Additional Logging tow_log_viewed_threads_fix: ' + cu.treatments.tow_log_viewed_threads_fix));
-
+/*
+Errors were refactored so this no longer works
                        var errorsInSession = _.union(yd.a.report.exception._errorsToReport,
                                yd.a.report.exception._previouslyReported);
                        if (errorsInSession.length > 0) {
@@ -311,6 +312,7 @@ yd.a.mdl.F.prototype.toString = function (verbose) {
 
                           console.group(yd.logd('(+) JS errors reported in this session'));
                             console.log(yd.logd(allErrorsString));
+*/
                           console.groupEnd();
                        }
 
